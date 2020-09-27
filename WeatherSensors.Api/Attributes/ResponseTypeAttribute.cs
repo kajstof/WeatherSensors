cb8610e222c4b34638fb5cf096bfaa9671d739eb
Krzysztof Krysiak <krysiak86@gmail.com>
2020-09-27 19:20:41 +0200

    Add ResponseTypeAttribute

---
+++ WeatherSensors.Api/Attributes/ResponseTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WeatherSensors.Api.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false)]
+    sealed class ResponseTypeAttribute : Attribute
+    {
+        public Type ResponseType { get; }
+
+        public ResponseTypeAttribute(Type type)
+        {
+            this.ResponseType = type;
+        }
+    }
+}
