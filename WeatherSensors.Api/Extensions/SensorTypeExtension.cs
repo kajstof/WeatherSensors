d76d0a81c81951a7e0e388da5da473c789b0f492
Krzysztof Krysiak <krysiak86@gmail.com>
2020-09-27 19:20:26 +0200

    Add SensorType

---
+++ WeatherSensors.Api/Extensions/SensorTypeExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WeatherSensors.Api.Models;
+using WeatherSensors.Api.Responses;
+
+namespace WeatherSensors.Api.Extensions
+{
+    public static class SensorTypeExtension
+    {
+        public static SensorType? ConvertToSensorType(this string text)
+        {
+            var sensorKeyValuePairs = typeof(SensorType).GetEnumValues().Cast<SensorType>()
+                .Select(x => new KeyValuePair<string, SensorType>(x.ToString().ToLower(), x));
+
+            var sensorsDict = new Dictionary<string, SensorType>(sensorKeyValuePairs);
+
+            if (sensorsDict.Keys.Any(x => x.Equals(text)))
+            {
+                return sensorsDict[text];
+            }
+
+            return null;
+        }
+
+        // TODO - Move this functionality to Attribute - ResponseTypeAttribute
+        public static Type GetResponseType(this SensorType sensorType)
+        {
+            switch (sensorType)
+            {
+                case SensorType.Humidity:
+                    return typeof(MeasurementHumidityResponse);
+                case SensorType.Rainfall:
+                    return typeof(MeasurementRainfallResponse);
+                case SensorType.Temperature:
+                    return typeof(MeasurementTemperatureResponse);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(sensorType), sensorType, null);
+            }
+        }
+    }
+}
---
+++ WeatherSensors.Api/Models/SensorType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using WeatherSensors.Api.Attributes;
+using WeatherSensors.Api.Responses;
+using WeatherSensors.Api.TypeConverters;
+
+namespace WeatherSensors.Api.Models
+{
+    [TypeConverter(typeof(SensorTypeConverter))]
+    public enum SensorType
+    {
+        [ResponseType(typeof(MeasurementHumidityResponse))]
+        Humidity,
+
+        [ResponseType(typeof(MeasurementRainfallResponse))]
+        Rainfall,
+
+        [ResponseType(typeof(MeasurementTemperatureResponse))]
+        Temperature
+    }
+}
