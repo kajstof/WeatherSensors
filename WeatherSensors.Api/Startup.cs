b8f0b985604e5b12f5ed63e1bafc3b2c262ff075
Krzysztof Krysiak <krysiak86@gmail.com>
2020-09-27 19:23:57 +0200

    Update Program & Startup

--- WeatherSensors.Api/Startup.cs
+++ WeatherSensors.Api/Startup.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WeatherSensors.Api.Services;
 
-namespace WeatherSensorsInformation
+namespace WeatherSensors.Api
 {
     public class Startup
     {
@@ -25,7 +29,18 @@ namespace WeatherSensorsInformation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var blobStorageConnectionString =
+                Configuration.GetConnectionString("SigmaIoTAzureBlobStorageConnectionString");
+            var blobContainerName = Configuration.GetValue<string>("AzureContainerName");
+
             services.AddControllers();
+            services.AddMediatR(typeof(Startup));
+            services.AddSingleton(x => new BlobContainerClient(blobStorageConnectionString, blobContainerName));
+            services.AddSingleton<IBlobService, BlobService>();
+            services.AddScoped<IMeasurementManagerService, MeasurementManagerService>();
+            services.AddScoped<IMeasurementFileNameGenerator, MeasurementFileNameGenerator>();
+            services.AddScoped<IFileCacheService, FileCacheService>();
+            services.AddScoped<IBlobDownloaderService, BlobDownloaderService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
