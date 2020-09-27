90833c317e5dc032148197eaf251add4e428806c
Krzysztof Krysiak <krysiak86@gmail.com>
2020-09-27 19:20:06 +0200

    Rename controller

---
+++ WeatherSensors.Api/Controllers/DevicesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WeatherSensors.Api.Models;
+using WeatherSensors.Api.Services;
+
+namespace WeatherSensors.Api.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class DevicesController : ControllerBase
+    {
+        private readonly ILogger<DevicesController> _logger;
+        private readonly IMeasurementManagerService _measurementManagerService;
+
+        public DevicesController(ILogger<DevicesController> logger,
+            IMeasurementManagerService measurementManagerService)
+        {
+            _logger = logger;
+            _measurementManagerService = measurementManagerService;
+        }
+
+        [HttpGet("getdata")]
+        [HttpGet("api/v1/devices/{deviceId}/data/{date}")]
+        [HttpGet("api/v1/devices/{deviceId}/data/{date}/{sensorType}")]
+        public async Task<IActionResult> DeviceDataGet(string deviceId, DateTime date, SensorType? sensorType)
+        {
+            _logger.LogInformation($"Getting data: {deviceId} | {date:yyy-MM-dd} | {sensorType.ToString()}");
+            var measurementData = await _measurementManagerService.GetData(deviceId, date, sensorType);
+            return Ok(measurementData);
+        }
+    }
+}
